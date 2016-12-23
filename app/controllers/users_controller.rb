3019b2e03d3455c2b6b1a7ed496bbf5bf3ab6fd7
Ivey Taylor <iveygtaylor@gmail.com>
2016-12-23 10:28:20 -0500

    Playing with switching Pirate language

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,12 @@ end
   end
 
   def new
+    @user = User.new
   end
 
   def create
-    if User.create(user_params)
+    @user = User.new(user_params)
+    if @user.save
       flash[:notice] = "User successfully created!"
       redirect_to users_path
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_many :tweets
+
+  validates :fname, presence: true
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_presence_of :password
+
   scope :today, lambda {where('DATE(created_at) = ?', Date.today)}
   scope :admin, -> { where(email: 'iveygtaylor@gmail.com').first }
 end
